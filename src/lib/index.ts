7c3ccce323a0a98b3b50365a13b148052eaae522
aquacalc <aquacalc@yahoo.com>
2025-07-13 19:11:54 -0400

    initial commit

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+// place files you want to import through the `$lib` alias in this folder.
+import Hero from "./components/Hero.svelte";
+import Button from "./components/Button.svelte";
+
+
+export {Hero, Button}
