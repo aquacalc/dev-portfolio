4724d07eb617a5ead647db65ba94958b691ab94d
aquacalc <aquacalc@yahoo.com>
2025-07-13 19:12:47 -0400

    add Hero section

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Button from "./Button.svelte";
+
+	//
+</script>
+
+<section class="hero-section default-margin">
+	<h3>Gazda vode</h3>
+  <div class="underscore mb-m"></div>
+
+  <h2 class='dark-grey'>Water chemistry</h2>
+  <h2 class='dark-grey'>as intuitive as</h2>
+  <h2>as Google Maps</h2>
+
+  <Button>from Hero</Button>
+</section>
+
+<style>
+  .hero-section {
+    padding-top: min(3.5rem, 6%);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import {Hero } from '$lib'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<Hero />
