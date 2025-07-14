82e84f53b76c297473c13f36d1b610d8a950fd89
aquacalc <aquacalc@yahoo.com>
2025-07-14 11:23:08 -0400

    update Hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import Button from './Button.svelte';
+	import { Button, SectionHeadline } from '$lib';
 
 	// const onclick = () => goto('/#contact-form');
 	function onclick() {
@@ -8,13 +8,16 @@
 	}
 </script>
 
-<section class="hero-section default-margin">
-	<h3>Gazda vode</h3>
-	<div class="underscore mb-m"></div>
+<!-- <SectionHeadline sectionName='Gazda'>Gazda vode</SectionHeadline> -->
 
+<section class="hero-section default-margin">
+  <h3>Welcome!</h3>
+  <div class="underscore mb-m"></div>
+
+	<!-- <h2 class="dark-grey">Aquaculture</h2> -->
 	<h2 class="dark-grey">Water chemistry</h2>
 	<h2 class="dark-grey">as intuitive as</h2>
-	<h2 class='mb-m'>as Google Maps</h2>
+	<h2 class="mb-m">Google Maps</h2>
 
 	<Button {onclick}>Let's get in contact!</Button>
 </section>
