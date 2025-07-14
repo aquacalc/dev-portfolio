a83572f1823dec2befa04a2c556868b36ec4b1f7
aquacalc <aquacalc@yahoo.com>
2025-07-13 21:40:07 -0400

    add mb-m

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
-	import Button from "./Button.svelte";
+	import { goto } from '$app/navigation';
+	import Button from './Button.svelte';
 
-	//
+	// const onclick = () => goto('/#contact-form');
+	function onclick() {
+		goto('/#contact-form');
+	}
 </script>
 
 <section class="hero-section default-margin">
 	<h3>Gazda vode</h3>
-  <div class="underscore mb-m"></div>
+	<div class="underscore mb-m"></div>
 
-  <h2 class='dark-grey'>Water chemistry</h2>
-  <h2 class='dark-grey'>as intuitive as</h2>
-  <h2>as Google Maps</h2>
+	<h2 class="dark-grey">Water chemistry</h2>
+	<h2 class="dark-grey">as intuitive as</h2>
+	<h2 class='mb-m'>as Google Maps</h2>
 
-  <Button>from Hero</Button>
+	<Button {onclick}>Let's get in contact!</Button>
 </section>
 
 <style>
-  .hero-section {
-    padding-top: min(3.5rem, 6%);
-  }
+	.hero-section {
+		padding-top: min(3.5rem, 6%);
+	}
 </style>
