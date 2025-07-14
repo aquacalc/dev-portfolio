838a83b538f8dd0fe0c584f4a94a8b70802ac371
aquacalc <aquacalc@yahoo.com>
2025-07-14 11:22:08 -0400

    update barrel file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
-import Hero from "./components/Hero.svelte";
-import Button from "./components/Button.svelte";
+import Hero from './components/Hero.svelte';
+import Button from './components/Button.svelte';
+import SectionHeadline from './components/SectionHeadline.svelte';
+import AboutMe from './components/AboutMe.svelte';
 
-
-export {Hero, Button}
+export { Hero, Button, SectionHeadline, AboutMe };
