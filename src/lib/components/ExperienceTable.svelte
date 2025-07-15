51d7909563286460f711c6e541c2085d0bb5175b
aquacalc <aquacalc@yahoo.com>
2025-07-15 10:13:54 -0400

    add dummy data

---
+++ src/lib/components/ExperienceTable.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  interface WorkExperience {
+    jobTitle: string;
+    company: string;
+    startDate: string;
+    endDate?: string;
+  }
+  const workExpeience: WorkExperience[] = [
+    {jobTitle: 'Bottle Washer',
+      company: 'Yadda',
+      startDate: '2002-03'
+    },
+    {jobTitle: 'Bottle Washer',
+      company: 'Yadda',
+      startDate: '2002-03',
+      endDate: '2018-08'
+    },
+    {jobTitle: 'Bottle Washer',
+      company: 'Yadda',
+      startDate: '2002-03',
+      endDate: '2018-08'
+    },
+    {jobTitle: 'Bottle Washer',
+      company: 'Yadda',
+      startDate: '2002-03',
+      endDate: '2018-08'
+    },
+  ]
+</script>
+
+<section class="default-margin work-experience mt-m">
+	<ul class="work-experience-list">
+    {#each workExpeience as {jobTitle, company}}
+      <h3>{jobTitle} - {company}</h3>
+    {/each}
+  </ul>
+	<h2 class="headline">
+		Past work <br /> Experience
+	</h2>
+</section>
