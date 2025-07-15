d17c2dc9eb8f75f87299e9acb56df0e3b4858325
aquacalc <aquacalc@yahoo.com>
2025-07-15 10:13:21 -0400

    add fe-export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ import Hero from './components/Hero.svelte';
 import Button from './components/Button.svelte';
 import SectionHeadline from './components/SectionHeadline.svelte';
 import AboutMe from './components/AboutMe.svelte';
+import ExperienceTable from './components/ExperienceTable.svelte';
 
-export { Hero, Button, SectionHeadline, AboutMe };
+export { Hero, Button, SectionHeadline, AboutMe, ExperienceTable };
